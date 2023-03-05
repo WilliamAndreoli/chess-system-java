88ce172c1d870dbe047316a973c1a0185b682175
William Andreoli <willandreoli33@gmail.com>
2023-03-05 00:07:50 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -26,4 +26,12 @@ public class Board {
     public void setColunms(int colunms) {
         this.colunms = colunms;
     }
+
+    public Piece piece(int row, int colunm){
+        return pieces[row][colunm];
+    }
+
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColunm()];
+    }
 }
